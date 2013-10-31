0364bf80b39de0ec9c051e41cef3426330b82e90
chadcolby <colbyc@spu.edu>
2013-10-30 23:04:06 -0700

    Initial Commit

---
+++ toDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  toDoList
+//
+//  Created by Chad D Colby on 10/30/13.
+//  Copyright (c) 2013 kramerica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FKAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FKAAppDelegate class]));
+    }
+}
